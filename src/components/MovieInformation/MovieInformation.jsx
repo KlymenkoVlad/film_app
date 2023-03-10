97850b18b9ed87e928f61b1dec5714b462cc229d
Vlad Klymenko <klymenvlad@gmail.com>
2023-03-10 15:45:38 +0200

    structure done

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MovieInformation() {
+  console.log('Movie information');
+
+  return (
+    <div>
+      MovieInformation
+    </div>
+  );
+}
+
+export default MovieInformation;
