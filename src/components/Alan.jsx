b3a469555c30e2bc91e6cd69784a0f8d18ccc261
Vlad Klymenko <klymenvlad@gmail.com>
2023-03-18 21:39:04 +0200

    Alan small fix

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
-import { fetchToken } from '../utils';
 import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils/index';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
 
-const useAlan = () => {
+function useAlan() {
   const { setMode } = useContext(ColorModeContext);
   const dispatch = useDispatch();
-  const history = useHistory();
+  const navigate = useNavigate();
 
   useEffect(() => {
     alanBtn({
@@ -18,13 +18,12 @@ const useAlan = () => {
       onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
         if (command === 'chooseGenre') {
           const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
-
           if (foundGenre) {
-            history.pushState('/');
+            navigate('/');
             dispatch(selectGenreOrCategory(foundGenre.id));
           } else {
             const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
-            history.pushState('/');
+            navigate('/');
             dispatch(selectGenreOrCategory(category));
           }
         } else if (command === 'changeMode') {
@@ -37,13 +36,13 @@ const useAlan = () => {
           fetchToken();
         } else if (command === 'logout') {
           localStorage.clear();
-          history.push('/');
+          navigate('/');
         } else if (command === 'search') {
           dispatch(searchMovie(query));
         }
       },
     });
   }, []);
-};
+}
 
 export default useAlan;
